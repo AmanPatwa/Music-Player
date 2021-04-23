87614c1a1b3ead49f7cc5e6bc948d87d38933370
AmanPatwa <amanpatwa999@gmail.com>
2021-04-23 14:51:53 +0530

    settings change

--- music_website/settings.py
+++ music_website/settings.py
@@ -116,6 +116,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+ALLOWED_HOSTS = ['music-player12.herokuapp.com', 'localhost', '127.0.0.1'] 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
